84cf77e0968eec4cf4d190913ac352a1b2a9655e
iCirav <22224223+iCirav@users.noreply.github.com>
2026-05-21 02:54:38 -0700

    Added ram

---
+++ ram.v
@@ -0,0 +1,21 @@
+`default_nettype none
+
+module ram (
+    input wire clk,
+    input wire [12:0] address,
+    input wire [7:0] data_in,
+    input wire write_enable,
+    output reg [7:0] data_out
+);
+
+    // 8kb of RAM
+    reg [7:0] memory [0:8191];
+
+    always @(posedge clk) begin
+        if (write_enable)
+            memory[address] <= data_in; 
+        
+        data_out <= memory[address];
+    end
+endmodule
+
