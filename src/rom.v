f03a05ac9efdb78bd44ee588f2bf905a4480515d
iCirav <22224223+iCirav@users.noreply.github.com>
2026-05-21 20:28:28 -0700

    Updated memory architecture

--- src/rom.v
+++ src/rom.v
@@ -1,24 +1,18 @@
 `default_nettype none
 
-module ram (
+module rom (
     input wire          CLK,
     input wire  [12:0]  address,
-    input wire  [7:0]   data_in,
-    input wire          write_enable,
     input wire          chip_enable,
     output reg  [7:0]   data_out
 );
 
-    // 6KB of RAM
-    reg [7:0] memory [0:6143];
+    // 2KB of ROM
+    reg [7:0] memory [0:2047];
 
     always @(posedge CLK) begin
         if (chip_enable) begin
-            if (write_enable)
-                memory[address] <= data_in; 
-        
             data_out <= memory[address];
         end
     end
 endmodule
-
